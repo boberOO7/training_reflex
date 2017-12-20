271119209ed9a0cdad92db57d1a649f885d66432
boberOO7 <33721308+boberOO7@users.noreply.github.com>
2017-12-21 00:02:42 +0200

    menu 1.0

--- tr/tr/menu.Designer.cs
+++ tr/tr/menu.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.No;
+            this.button1.Location = new System.Drawing.Point(225, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Game1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.game1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Game2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.game2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 84);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "menu";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- tr/tr/menu.cs
+++ tr/tr/menu.cs
@@ -12,9 +12,31 @@ namespace tr
 {
     public partial class menu : Form
     {
+        game1 game1 = new game1();
+        game2 game = new game2();
+
         public menu()
         {
             InitializeComponent();
+        }
+
+
+        private void game2_Click(object sender, EventArgs e)
+        {
+            game.Show();
+            this.Hide();
+        }
+
+        private void game1_Click(object sender, EventArgs e)
+        {
+            game1.Show();
+            this.Hide();
+            game1.startTimer.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
